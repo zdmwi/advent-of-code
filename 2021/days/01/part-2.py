fc38b812d337fea46b24c9e80eb4d7ddc607921f
Zidane Wright <zidane.wright@speurgroup.com>
2021-12-02 02:27:53 -0500

    Simplify day 1 solution

--- 2021/days/01/part-2.py
+++ 2021/days/01/part-2.py
@@ -1,29 +1,11 @@
 import sys
 from typing import List
 
-def get_prefix_sum(lst: List[int]):
-  n = len(lst)
-  prefix_sum = [0 for _ in range(n + 1)]
-
-  for i in range(1, n+1):
-    prefix_sum[i] = prefix_sum[i-1] + lst[i-1]
-
-  return prefix_sum
-
-
-def count_increases(depths: List[int]):
-  psum = get_prefix_sum(depths)
-
+def count_increases(depths: List[int], window_size: int):
   increases = 0
-  prev = psum[3] # the sum of the first window
-  second_window_idx = 4 # the starting point of the second window
-  for i in range(second_window_idx, len(psum)):
-    current = psum[i] - psum[i-3]
-    if (current > prev):
+  for i in range(window_size, len(depths)):
+    if depths[i] > depths[i-window_size]:
       increases += 1
-
-    prev = current
-
   return increases
 
 
@@ -31,5 +13,5 @@ if __name__ == '__main__':
   lines = sys.stdin.readlines()
   depths = list(map(int, [line.strip() for line in lines]))
   
-  ans = count_increases(depths)
+  ans = count_increases(depths, 3)
   print(ans)
